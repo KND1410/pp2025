b2f29b35e339f25b8875344035a960937be126f9
Nguyen Dang Khoi <khoind1410@gmail.com>
2025-12-29 00:07:09 +0700

    Prac5+6_presistent+pickle

---
+++ p5+p6/domain/__init__.py
@@ -0,0 +1,3 @@
+from .student import number_student, student_iD, student_name, student_birth, list_student
+from .course import number_course, course_iD, course_name, list_course, course_credit
+from .mark import mark_student, list_mark
